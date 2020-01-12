60802179f507abdbc6bbfcdaa8f730fd4da74076
Hiroharu Hara <hh.25799623@gmail.com>
2020-01-12 15:49:45 +0900

    コンフリクトを解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'types/index'
+  get 'types/show'
+  get 'types/new'
+  get 'types/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
